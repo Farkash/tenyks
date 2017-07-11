20113316c0748953361c883a30ececac70b7630b
STEVE FARKAS <Steve@STEVEs-MacBook-Pro.local>
2017-07-11 07:55:28 -0400

    almost

--- src/school-review-by-county.py
+++ src/school-review-by-county.py
@@ -1,26 +1,15 @@
 import urllib2
 from bs4 import BeautifulSoup
 import pandas
-
 base_url = "https://www.privateschoolreview.com"
-
-state_list = ["alabama", "alaska",
-              "arizona", "arkansas",
-              "california", "colorado",
-              "connecticut", "delaware", "district-of-columbia",
-              "florida", "georgia", "hawaii",
-              "idaho", "illinois", "indiana", "iowa", "kansas",
-              "kentucky", "louisiana",
-              "maine", "maryland", "massachusetts", "michigan",
-              "minnesota", "mississippi",
-              "missouri", "montana", "nebraska", "nevada",
-              "new-hampshire", "new-jersey",
-              "new-mexico", "new-york", "north-carolina",
-              "north-dakota", "ohio", "oklahoma",
-              "oregon", "pennsylvania", "rhode-island",
-              "south-carolina", "south-dakota",
-              "tennessee", "texas", "utah", "vermont",
-              "virginia", "washington",
+state_list = ["alabama", "alaska", "arizona", "arkansas", "california", "colorado",
+              "connecticut", "delaware", "district-of-columbia", "florida", "georgia", "hawaii",
+              "idaho", "illinois", "indiana", "iowa", "kansas", "kentucky", "louisiana",
+              "maine", "maryland", "massachusetts", "michigan", "minnesota", "mississippi",
+              "missouri", "montana", "nebraska", "nevada", "new-hampshire", "new-jersey",
+              "new-mexico", "new-york", "north-carolina", "north-dakota", "ohio", "oklahoma",
+              "oregon", "pennsylvania", "rhode-island", "south-carolina", "south-dakota",
+              "tennessee", "texas", "utah", "vermont", "virginia", "washington",
               "west-virginia", "wisconsin", "wyoming"
               ]
 # declare lists
@@ -64,12 +53,8 @@ for st in state_list:
     opened_state = urllib2.urlopen(url)
     soup = BeautifulSoup(opened_state, "lxml")
 
-# soup is one object here, not a list of objects, so we
-# can apply the find_all function to it once. Don't need to loop at all
     target_divs = soup.find_all("div", class_="school-type-list-text")
 
-    # since target_divs is a list of divs, we need to loop over each item to
-    # perform any functions on the entire list
     county = []
     county_dirs = []
     for div in target_divs:
@@ -323,7 +308,8 @@ for st in state_list:
                         if f == (len(detail_list)-1):
                             extracirricular = ''
                             extra_final.append(extracirricular)
-                        
+
+
 big_frame['State Full'] = state_full_final
 big_frame['County'] = county_final
 big_frame['School Name'] = school_name_final
@@ -349,11 +335,6 @@ big_frame['Acceptance Rate'] = acceptance_final
 big_frame['Total Sports Offered'] = sports_final
 big_frame['Total Extracurriculars'] = extra_final
 
-# big_frame.to_csv("state_summary.csv",
-#                  encoding='utf-8', index=False)
-
-
-
-
-
+big_frame.to_csv("state_summary.csv",
+                 encoding='utf-8', index=False)
 
